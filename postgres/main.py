e52db65e5661d5d424fe4566212b35bf196b1fe1
Rustam Sayfutdinov <rstm.sf@gmail.com>
2018-05-15 01:40:04 +0300

    Создаются и заполняются таблицы.

--- postgres/main.py
+++ postgres/main.py
@@ -1,3 +1,4 @@
+import json
 import psycopg2
 import sys
 
@@ -5,24 +6,89 @@ sys.path.append('../')
 from config import config
 
 
-def connect():
-    """ Connect to the PostgreSQL database server """
+def create_db():
+    commands = (
+        """CREATE TABLE reg (
+            id SERIAL PRIMARY KEY,
+            name CHAR(64) NOT NULL);""",
+        """CREATE TABLE "group" (
+            id SERIAL PRIMARY KEY,
+            name CHAR(64) NOT NULL);""",
+        """CREATE TABLE category (name CHAR(64) PRIMARY KEY UNIQUE);""",
+        """CREATE TABLE gender (name CHAR(64) PRIMARY KEY UNIQUE);""",
+        """CREATE TABLE crimestatsocial (
+            id SERIAL PRIMARY KEY,
+            reg_id INTEGER NOT NULL,
+            year INTEGER NOT NULL,
+            group_id INTEGER NOT NULL,
+            category CHAR(64) NOT NULL,
+            gender CHAR(64) NOT NULL,
+            value INTEGER NOT NULL,
+            FOREIGN KEY (reg_id)
+                REFERENCES reg (id)
+                ON UPDATE CASCADE ON DELETE CASCADE,
+            FOREIGN KEY (group_id)
+                REFERENCES "group" (id)
+                ON UPDATE CASCADE ON DELETE CASCADE,
+            FOREIGN KEY (category)
+                REFERENCES category (name)
+                ON UPDATE CASCADE ON DELETE CASCADE,
+            FOREIGN KEY (gender)
+                REFERENCES gender (name)
+                ON UPDATE CASCADE ON DELETE CASCADE);""")
     conn = None
     try:
         params = config()
-
-        print('Connecting to the PostgreSQL database...')
         conn = psycopg2.connect(**params)
-
         cur = conn.cursor()
-
-        print('PostgreSQL database version:')
-        cur.execute('SELECT version()')
-
-        db_version = cur.fetchone()
-        print(db_version)
-
+        for command in commands:
+            cur.execute(command)
         cur.close()
+        conn.commit()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+            print('Database connection closed.')
+
+
+def insert_table_reg():
+    reg = set()
+    group = set()
+    category = set()
+    gender = set()
+    crimestatsocial = list()
+    for line in open('../crimestatsocial_final.json', 'r'):
+        j = json.loads(line)
+        reg.add((j["reg_id"], j["reg_name"]))
+        group.add((j["group_id"], j["group_name"]))
+        category.add((j["category"], ))
+        gender.add((j["gender"], ))
+        crimestatsocial.append(
+            (j["reg_id"], j["year"], j["group_id"],
+            j["category"], j["gender"], j["value"], ))
+    conn = None
+    try:
+        params = config()
+        conn = psycopg2.connect(**params)
+        cur = conn.cursor()
+        for item in reg:
+            cur.execute("""INSERT INTO reg VALUES (%s, %s)""", item)
+        for item in group:
+            cur.execute("""INSERT INTO "group" VALUES (%s, %s)""", item)
+        for item in category:
+            cur.execute("""INSERT INTO category VALUES (%s)""", item)
+        for item in gender:
+            cur.execute("""INSERT INTO gender VALUES (%s)""", item)
+        for item in crimestatsocial:
+            cur.execute("""
+                INSERT INTO
+                    crimestatsocial
+                    (reg_id, year, group_id, category, gender, value)
+                VALUES (%s, %s, %s, %s, %s, %s)""", item)
+        cur.close()
+        conn.commit()
     except (Exception, psycopg2.DatabaseError) as error:
         print(error)
     finally:
@@ -32,4 +98,5 @@ def connect():
 
 
 if __name__ == '__main__':
-    connect()
+    create_db()
+    insert_table_reg()
